7aa9be054d88ab2d46d804653b952d3c1fcf4080
Siddhant Topiwala <sidd.topiwala02@gmail.com>
2024-08-20 17:04:28 -0400

    remove unncessary section from landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
       />
       {/* <Video /> */}
       {/* <AboutSectionOne /> */}
-      <AboutSectionTwo />
+      {/* <AboutSectionTwo /> */}
       {/* <Testimonials /> */}
       {/* <Pricing /> */}
       {/* <Contact /> */}

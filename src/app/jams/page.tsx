05567bcf6db02ab5ef6fd3e05caea66172007305
Siddhant Topiwala <sidd.topiwala02@gmail.com>
2024-08-24 19:08:05 -0400

    add discord link to jams page

--- src/app/jams/page.tsx
+++ src/app/jams/page.tsx
@@ -13,7 +13,8 @@ const JamsPage = () => {
   Jams are weekly* run jam sessions open to all students at the University, regardless of Faculty. <br></br><br></br>
   Coming to jams is a great way to meet other musicians, develop your skills, and have fun playing music. <br></br><br></br>
   Currently, Jams are being run on &lt;TBD&gt; in Myhal 041 (basement). <br></br><br></br>
-  *Jams may not happen every week, if you are interested, please join the discord, where we make the annoucements for jams.
+  *Jams may not happen every week, if you are interested, please join the <a href="https://discord.gg/nQ6C2BMvPe" className="text-primary"><u>discord</u></a>, 
+  where we make the annoucements for jams.
   </p>
 
   return (

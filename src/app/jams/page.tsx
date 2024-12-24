15b17da0aaaeaa8733ed254c9ccef1b3cb1981ea
Siddhant Topiwala <sidd.topiwala02@gmail.com>
2024-12-23 23:11:57 -0500

    refactor: remove discord link from site

--- src/app/jams/page.tsx
+++ src/app/jams/page.tsx
@@ -13,7 +13,7 @@ const JamsPage = () => {
   Jams are weekly* run jam sessions open to all students at the University, regardless of Faculty. <br></br><br></br>
   Coming to jams is a great way to meet other musicians, develop your skills, and have fun playing music. <br></br><br></br>
   Currently, Jams are being run on Fridays from 6:00 - 8:00 pm in Myhal 041 (basement). <br></br><br></br>
-  *Jams may not happen every week, if you are interested, please join the <a href="https://discord.gg/nQ6C2BMvPe" className="text-primary"><u>discord</u></a>, 
+  *Jams may not happen every week, if you are interested, please email stageband@g.skule.ca to request to join the discord, 
   where we make the annoucements for jams.
   </p>
 
